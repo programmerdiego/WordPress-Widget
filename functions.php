405c443acdd90dbaff7f54f18ba7433243940972
Diego Sanchez <sanchezdiego@email.com>
2018-08-07 13:18:05 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,3 +3,8 @@
 add_action('wp_enqueue_scripts', function() {
   wp_enqueue_style('css-of-parent', get_template_directory_uri() . '/style.css');
 });
+
+require_once 'mywidget.php';
+add_action('widgets_init', function() {
+    register_widget('MyWidget');
+});
